03114f09f119417c3242a9fb6e0b722676aedf38
Oscar Dowson <odow@users.noreply.github.com>
2022-12-22 14:01:07 +1300

    Fix when input directories are not found (#20)
    * Fix when input directories are not found
    
    * Update main.jl
    
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>
    
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>

--- main.jl
+++ main.jl
@@ -9,8 +9,12 @@ using CoverageTools
 directories = get(ENV, "INPUT_DIRECTORIES", "src,ext")
 dirs = filter!(!isempty, split(directories, ","))
 for dir in dirs
-    isdir(dir) || error("directory \"$dir\" not found!")
+    if dir == "ext"
+        continue  # Silently skip this directory
+    elseif !isdir(dir)
+        error("directory \"$dir\" not found!")
+    end
 end
-
+filter!(isdir, dirs)
 pfs = mapreduce(process_folder, vcat, dirs)
 LCOV.writefile("lcov.info", pfs)
